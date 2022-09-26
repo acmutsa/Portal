9875cdad8a2478600cdba64530fbef407fa1e446
Lermatroid <liam@murray.to>
2022-09-26 04:06:27 -0500

    Add Event Header Hyrdration

--- src/components/events/EventHeader.tsx
+++ src/components/events/EventHeader.tsx
@@ -1,18 +1,34 @@
 import { FunctionComponent } from "react";
 import { BsFillCalendarEventFill, BsClockFill, BsPinMapFill } from "react-icons/bs";
 
-const EventHeader: FunctionComponent = () => {
+interface EventHeaderProps {
+	title: string;
+	imageURL: string;
+	hostOrg: string;
+	startDate: Date;
+	endDate: Date;
+	location: string;
+}
+
+const EventHeader: FunctionComponent<EventHeaderProps> = ({
+	title,
+	imageURL,
+	hostOrg,
+	startDate,
+	endDate,
+	location,
+}) => {
 	return (
 		<div className="bg-white mx-auto max-w-[1200px] grid grid-cols-2 min-h-[400px] rounded-xl">
 			<div className="flex items-center justify-center overflow-hidden rounded-l-xl bg-cover bg-no-repeat bg-[url('https://se-images.campuslabs.com/clink/images/fed0a19f-b07f-48af-945f-a87da0b33e109d180e36-1f7b-4c58-b1e7-b2a57fc1cf9f.png?preset=large-w')]"></div>
 			<div className="flex flex-col justify-center p-[5px]">
-				<h1 className="text-5xl font-extrabold font-raleway">Intro To Tech</h1>
-				<p className="font-opensans font-bold">Hosted by ACM</p>
+				<h1 className="text-5xl font-extrabold font-raleway">{title}</h1>
+				<p className="font-opensans font-bold">Hosted by {hostOrg}</p>
 				<br></br>
 				<br></br>
 				<p className="font-semibold flex items-center">
 					<BsFillCalendarEventFill className="mr-[5px]" />
-					9/25/22
+					{startDate.toLocaleDateString()}
 				</p>
 				<p className="font-semibold flex items-center">
 					<BsClockFill className="mr-[5px]" />
--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -9,9 +9,19 @@ const EventView: NextPage = () => {
 
 	return (
 		<div className="page-view pt-[20px]">
-			<EventHeader />
+			<EventHeader
+				title="Intro To Tech"
+				imageURL="https://se-images.campuslabs.com/clink/images/fed0a19f-b07f-48af-945f-a87da0b33e109d180e36-1f7b-4c58-b1e7-b2a57fc1cf9f.png?preset=large-w"
+				hostOrg="ACM"
+				startDate={new Date("Sun Sep 28 2022 04:15:04 GMT-0500 (Central Daylight Time)")}
+				endDate={new Date("Sun Sep 30 2022 04:15:04 GMT-0500 (Central Daylight Time)")}
+				location="Retama SU"
+			/>
 			<br />
-			<EventDescription description="This is a event!" calanderLink="https://acmutsa.org/" />
+			<EventDescription
+				description="This is a event! It has some other data around it."
+				calanderLink="https://acmutsa.org/"
+			/>
 		</div>
 	);
 };
