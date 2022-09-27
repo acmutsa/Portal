b0bb3c8e5b24742ead17de0d371f38c430df9680
Liam Murray <liam@murray.to>
2022-09-27 09:38:37 -0500

    Add Submit Btn Margin

--- src/components/admin/NewEventView.tsx
+++ src/components/admin/NewEventView.tsx
@@ -145,7 +145,7 @@ const NewEventView: FunctionComponent = () => {
 						</>
 					) : null}
 				</div>
-				<button className="bg-primary-lighter text-white h-[50px] w-[150px] rounded-xl font-bold flex items-center justify-center align-center">
+				<button className="bg-primary-lighter text-white h-[50px] w-[150px] rounded-xl font-bold flex items-center justify-center align-center mt-[15px]">
 					Create Event <BsFillArrowRightCircleFill className="ml-[5px]" />
 				</button>
 			</form>
