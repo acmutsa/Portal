9d0ecf276b658d4ace751bb09f83737cd510a811
Liam Murray <liam@murray.to>
2022-10-09 06:03:52 -0500

    Start Bringing Functionality Back In

--- src/pages/api/webhooks/typeform.ts
+++ src/pages/api/webhooks/typeform.ts
@@ -1,117 +1,115 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { parse } from "path";
 import { z } from "zod";
 import { prisma } from "../../../server/db/client";
 
-// const memberItem = z.object({
-// 	answers: z.tuple([
-// 		z.object({
-// 			type: z.string(),
-// 			text: z.string(),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			email: z.string(),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			text: z.string(),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			text: z.string(),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			choice: z.object({ label: z.string() }),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			date: z.string(),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			date: z.string(),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			choices: z.object({
-// 				labels: z.array(z.string()),
-// 			}),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			choices: z.object({
-// 				labels: z.array(z.string()),
-// 			}),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			choices: z.object({
-// 				labels: z.array(z.string()),
-// 			}),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			choice: z.object({ label: z.string() }),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			choice: z.object({ label: z.string() }),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			text: z.string(),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			text: z.string(),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			text: z.string(),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 		z.object({
-// 			type: z.string(),
-// 			text: z.string(),
-// 			field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-// 		}),
-// 	]),
-// });
-
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	// if (req.method == "POST") {
-	// 	// const parsedData = memberItem.safeParse(req.body.form_response.answers);
+	if (req.method == "POST") {
+		const memberItem = z.object({
+			answers: z.tuple([
+				z.object({
+					type: z.string(),
+					text: z.string(),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					email: z.string(),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					text: z.string(),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					text: z.string(),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					choice: z.object({ label: z.string() }),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					date: z.string(),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					date: z.string(),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					choices: z.object({
+						labels: z.array(z.string()),
+					}),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					choices: z.object({
+						labels: z.array(z.string()),
+					}),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					choices: z.object({
+						labels: z.array(z.string()),
+					}),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					choice: z.object({ label: z.string() }),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					choice: z.object({ label: z.string() }),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					text: z.string(),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					text: z.string(),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					text: z.string(),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+				z.object({
+					type: z.string(),
+					text: z.string(),
+					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				}),
+			]),
+		});
 
-	// 	// if (parsedData.success) {
-	// 	// 	const newMember = await prisma.member.create({
-	// 	// 		data: {
-	// 	// 			name: parsedData.data.answers[0].text,
-	// 	// 		},
-	// 	// 	});
-	// 	// } else {
-	// 	// 	res.status(400);
-	// 	// }
+		const parsedData = memberItem.safeParse(req.body.form_response.answers);
 
-	// 	// console.log(JSON.stringify(req.body));
-	// 	return res.status(200);
-	// } else {
-	// 	return res.status(405);
-	// }
+		if (parsedData.success) {
+			// const newMember = await prisma.member.create({
+			// 	data: {
+			// 		name: parsedData.data.answers[0].text,
+			// 	},
+			// });
+			console.log("Ayyyy!");
+			res.status(200);
+		} else {
+			res.status(400);
+		}
+	} else {
+		return res.status(405);
+	}
 	return res.status(200).json({});
 };
