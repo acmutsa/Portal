785d677f0669f756171ae25187d8034134b76c20
Liam Murray <liam@murray.to>
2022-10-09 01:51:04 -0500

    Stringify Typeform Data

--- src/pages/api/webhooks/typeform.ts
+++ src/pages/api/webhooks/typeform.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method == "POST") {
-		console.log(req.body);
+		console.log(JSON.stringify(req.body));
 		res.status(200);
 	} else {
 		res.status(405);
