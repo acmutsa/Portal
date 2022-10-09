b652184da6071a602c4e3327d79860c7678586a7
Liam Murray <liam@murray.to>
2022-10-09 01:16:55 -0500

    Update Typeform ID

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -6,7 +6,7 @@ const Join: NextPage = () => {
 	return (
 		<div className="page-view">
 			<div className="w-full max-w-[1024px] h-full flex flex-col items-center mx-auto">
-				<Widget className="w-full h-full" opacity={0} id="j7x46KIr" />
+				<Widget className="w-full h-full" opacity={0} id="UXDf2pFZ" />
 			</div>
 		</div>
 	);
