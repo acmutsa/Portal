b137edb28a5c5cf561f6171732485f171a00c4ec
Liam Murray <liam@murray.to>
2022-10-10 01:31:21 -0500

    Log Body Item

--- src/pages/api/webhooks/typeform.ts
+++ src/pages/api/webhooks/typeform.ts
@@ -99,6 +99,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		console.log(JSON.stringify(req.body));
 		console.log("===========");
 
+		console.log("===========");
+		console.log(JSON.stringify(req?.body?.form_response?.answers));
+		console.log("===========");
+
 		const parsedData = memberItem.safeParse(req?.body?.form_response?.answers);
 
 		if (parsedData.success) {
