b55300f1e7581e36db95cb0fe58d238ffad6451d
Liam Murray <liam@murray.to>
2022-10-10 11:01:02 -0500

    Fix Zod Validator

--- src/pages/api/webhooks/typeform.ts
+++ src/pages/api/webhooks/typeform.ts
@@ -4,96 +4,94 @@ import { prisma } from "../../../server/db/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method == "POST") {
-		const memberItem = z.object({
-			answers: z.tuple([
-				z.object({
-					type: z.string(),
-					text: z.string(),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+		const memberItem = z.tuple([
+			z.object({
+				type: z.string(),
+				text: z.string(),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				email: z.string(),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				text: z.string(),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				text: z.string(),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				choice: z.object({ label: z.string() }),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				date: z.string(),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				date: z.string(),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				choices: z.object({
+					labels: z.array(z.string()),
 				}),
-				z.object({
-					type: z.string(),
-					email: z.string(),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				choices: z.object({
+					labels: z.array(z.string()),
 				}),
-				z.object({
-					type: z.string(),
-					text: z.string(),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				choices: z.object({
+					labels: z.array(z.string()),
 				}),
-				z.object({
-					type: z.string(),
-					text: z.string(),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					choice: z.object({ label: z.string() }),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					date: z.string(),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					date: z.string(),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					choices: z.object({
-						labels: z.array(z.string()),
-					}),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					choices: z.object({
-						labels: z.array(z.string()),
-					}),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					choices: z.object({
-						labels: z.array(z.string()),
-					}),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					choice: z.object({ label: z.string() }),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					choice: z.object({ label: z.string() }),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					text: z.string(),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					text: z.string(),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					text: z.string(),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-				z.object({
-					type: z.string(),
-					text: z.string(),
-					field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
-				}),
-			]),
-		});
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				choice: z.object({ label: z.string() }),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				choice: z.object({ label: z.string() }),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				text: z.string(),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				text: z.string(),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				text: z.string(),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+			z.object({
+				type: z.string(),
+				text: z.string(),
+				field: z.object({ id: z.string(), type: z.string(), ref: z.string() }),
+			}),
+		]);
 
 		console.log("=========== body:");
 		console.log(JSON.stringify(req.body));
