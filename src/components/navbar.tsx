dee9456a57948a5b7561470834e84bf3f3f985dc
Liam Murray <liam@murray.to>
2022-10-11 13:47:29 -0500

    Add Check-in Page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,16 @@
 import { FunctionComponent } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar: FunctionComponent = () => {
 	return (
 		<div className="h-[72px] w-full bg-primary-darker flex items-center px-[5px]">
-			<Image width={42} height={42} src={"/img/logo.png"} />
-			<h1 className="text-white font-opensans text-xl font-semibold ml-[5px]">Members</h1>
+			<Link href={"/"}>
+				<div className="flex items-center cursor-pointer">
+					<Image width={42} height={42} src={"/img/logo.png"} />
+					<h1 className="text-white font-opensans text-xl font-semibold ml-[5px]">Members</h1>
+				</div>
+			</Link>
 		</div>
 	);
 };
