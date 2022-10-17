dadc1fd490ecd34040c7c0b13146e1ec7916d7d6
Liam Murray <liam@murray.to>
2022-10-17 17:43:32 -0500

    Add Member Status and Update Page

---
+++ src/pages/member/status.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Status: NextPage = () => {
+	return (
+		<div className="page-view">
+			<h1>View Member Status</h1>
+		</div>
+	);
+};
+
+export default Status;
---
+++ src/pages/member/update.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Status: NextPage = () => {
+	return (
+		<div className="page-view">
+			<h1>Update Member</h1>
+		</div>
+	);
+};
+
+export default Status;
