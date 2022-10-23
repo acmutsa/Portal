adc66753f9510371d94cc7b95c09fbd7649f151e
Xevion <xevioni@yandex.com>
2022-10-22 22:13:18 -0500

    Fix next.config.mjs compile imports, remove unnecessary prettier configurations
    Removed rangeStart/rangeEnd configurations left over from Liam's configuration

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -3,8 +3,8 @@
  * This file is included in `/next.config.mjs` which ensures the app isn't built with invalid env vars.
  * It has to be a `.mjs`-file to be imported there.
  */
-import { serverSchema } from "@/env/schema.mjs";
-import { env as clientEnv, formatErrors } from "@/env/client.mjs";
+import { serverSchema } from "./schema.mjs";
+import { env as clientEnv, formatErrors } from "./client.mjs";
 
 const _serverEnv = serverSchema.safeParse(process.env);
 
