9e17fcead67a5663ba231ceb04f299741e867876
Xevion <xevioni@yandex.com>
2022-10-24 13:43:39 -0500

    Cleanup and restructure navbar CSS, add Register link
    Noticed that className on Link was doing nothing. Divided the navbar into 'sections', and then moved their content around within that.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,18 +4,21 @@ import Link from "next/link";
 
 const Navbar: FunctionComponent = () => {
 	return (
-		<div className="h-[72px] w-full bg-primary-darker font-opensans drop-shadow-lg text-white text-xl flex items-center justify-evenly px-[5px]">
-			<Link className="font-bold" href={"/events"}>
-				Events
-			</Link>
-			<Link href={"/"}>
-				<div className="relative top-[1.2rem] hover:translate-y-1 cursor-pointer hover:scale-[110%] transition-transform">
-					<Image width={100} height={100} quality={100} loading={"eager"} src={"/img/logo.png"} />
-				</div>
-			</Link>
-			<Link className="font-bold" href={"/login"}>
-				Login
-			</Link>
+		<div className="h-[72px] w-full bg-primary-darker font-inter drop-shadow-lg text-white text-xl flex items-center">
+			<div className="flex-1 text-center">
+				<Link href={"/events"}>Events</Link>
+			</div>
+			<div className="flex-2">
+				<Link href={"/"}>
+					<div className="flex justify-center relative top-[1.2rem] hover:translate-y-1 cursor-pointer hover:scale-[110%] transition-transform">
+						<Image width={100} height={100} quality={100} loading={"eager"} src={"/img/logo.png"} />
+					</div>
+				</Link>
+			</div>
+			<div className="flex-1 flex justify-evenly">
+				<Link href={"/login"}>Login</Link>
+				<Link href={"/register"}>Register</Link>
+			</div>
 		</div>
 	);
 };
