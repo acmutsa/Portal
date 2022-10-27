37f1d3331fc3893dc81eacd13fc5be47014fb15a
Xevion <xevioni@yandex.com>
2022-10-26 23:05:37 -0500

    Add intelligent navbar underline highlighting akin to primary website

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,14 +2,37 @@ import { FunctionComponent } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useGlobalContext } from "@/components/state/global";
+import { useRouter } from "next/router";
+
+interface HighlightProps {
+	router: any;
+	route: string;
+	href?: string;
+	children: JSX.Element | JSX.Element[] | string;
+}
+
+const NavbarItem: FunctionComponent<HighlightProps> = ({ router, route, children, href }) => {
+	const highlight = route == router.route;
+	return (
+		<Link href={href ?? route}>
+			<a className={`under-hover ${highlight ? "force" : ""}`}>{children}</a>
+		</Link>
+	);
+};
 
 const Navbar: FunctionComponent = () => {
 	const [globalState, setGlobalState] = useGlobalContext();
+	const router = useRouter();
 
 	return (
 		<div className="h-[72px] w-full bg-primary-darker font-inter drop-shadow-lg text-white text-xl flex items-center">
-			<div className="flex-1 text-center">
-				<Link href={"/events"}>Events</Link>
+			<div className="flex-1 flex justify-evenly text-center">
+				<NavbarItem router={router} route={"/"}>
+					Home
+				</NavbarItem>
+				<NavbarItem router={router} route={"/events"}>
+					Events
+				</NavbarItem>
 			</div>
 			<div className="flex-2">
 				<Link href={"/"}>
@@ -21,19 +44,21 @@ const Navbar: FunctionComponent = () => {
 			<div className="flex-1 flex justify-evenly">
 				{globalState.loggedIn
 					? [
-							<Link key={1} href={"/member/status"}>
+							<NavbarItem key={1} router={router} route={"/member/status"}>
 								Status
-							</Link>,
+							</NavbarItem>,
 							<Link key={2} href={"/logout"}>
-								Logout
+								<a className="under-hover">Logout</a>
 							</Link>,
 					  ]
 					: [
-							<Link key={1} href={"/login"}>
+							<NavbarItem key={1} router={router} route={"/login"}>
 								Login
-							</Link>,
-							<Link key={3} href={"/register"}>
-								Register
+							</NavbarItem>,
+							<Link key={2} href={"/register"}>
+								<NavbarItem router={router} route={"/register"}>
+									Register
+								</NavbarItem>
 							</Link>,
 					  ]}
 			</div>
