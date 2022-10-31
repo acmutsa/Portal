bbfa4f6d794ef1749443b1022f0c306b0ec361ac
Xevion <ryanchwalters@gmail.com>
2022-10-30 20:09:18 -0500

    Add first iteration of dashboard analytics, tRPC grouped checkin statistics route

--- src/server/router/events.ts
+++ src/server/router/events.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { createRouter } from "@/server/router/context";
+import { sum } from "@/utils/helpers";
 
 export const eventsRouter = createRouter()
 	.query("getCurrent", {
@@ -24,5 +25,62 @@ export const eventsRouter = createRouter()
 					eventStart: "asc",
 				},
 			});
+		},
+	})
+	.query("getGroupedCheckins", {
+		input: z.object({
+			startDate: z.date(),
+		}),
+		async resolve({ input, ctx }) {
+			const events = (
+				await ctx.prisma.event.findMany({
+					where: {
+						eventEnd: {
+							gte: input.startDate,
+						},
+					},
+					include: {
+						_count: { select: { checkins: true } },
+					},
+				})
+			)
+				// .filter((event) => event._count.checkins > 0)
+				.map((event) => {
+					// Converts the event's end date to the first day of the week (monday)
+					const week = event.eventEnd;
+					week.setDate(week.getDate() - ((week.getDay() + 6) % 7));
+
+					return {
+						// name: event.name,
+						// date: event.eventEnd,
+						week: `${week.getMonth() + 1}/${week.getDate()}`,
+						count: event._count.checkins,
+					};
+				});
+
+			const now = new Date();
+			const orderedWeekKeys = [input.startDate];
+			while (1) {
+				const nextWeek = new Date(orderedWeekKeys.at(-1)!);
+				nextWeek.setDate(nextWeek.getDate() + 7);
+
+				if (nextWeek < now) orderedWeekKeys.push(nextWeek);
+				else break;
+			}
+
+			const grouped = events.reduce(function (allEvents, event) {
+				// Make sure an array is available
+				allEvents[event.week] = allEvents[event.week] || [];
+				// Add the event to the array with it's group
+				allEvents[event.week].push(event);
+				return allEvents;
+			}, Object.create(null));
+
+			return orderedWeekKeys
+				.map((week) => `${week.getMonth() + 1}/${week.getDate()}`)
+				.map((week) => ({
+					label: week,
+					count: (grouped[week] || []).map((week) => week.count).reduce(sum, 0),
+				}));
 		},
 	});
