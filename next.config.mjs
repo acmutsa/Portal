1b1a46caaa7f9b03c02addcf7ff0a8cdb1f852cc
Xevion <ryanchwalters@gmail.com>
2022-11-03 18:27:15 -0500

    Disable swcMinify fixing ChartJS ticks build issue

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ function defineNextConfig(config) {
 
 export default defineNextConfig({
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
   i18n: {
     locales: ["en"],
