ee5facebd00ceca55d0222b73349e16e13d5412f
Xevion <ryanchwalters@gmail.com>
2022-11-05 19:15:07 -0500

    Optimize Navbar CSS class usages, remove unused/old checkin imports

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,9 +32,7 @@ const NavbarItem: FunctionComponent<HighlightProps> = ({
 
 	return (
 		<Link href={href ?? route}>
-			<a className={`under-hover ${highlight ? "force" : ""} mx-[15px] font-sans text-[14px]`}>
-				{children}
-			</a>
+			<a className={`under-hover mx-4 ${highlight ? "force" : ""}`}>{children}</a>
 		</Link>
 	);
 };
@@ -43,43 +41,43 @@ const Navbar: FunctionComponent = () => {
 	const [globalState, setGlobalState] = useGlobalContext();
 	const router = useRouter();
 
-	let right_navbar_elements = globalState.loggedIn
+	let dynamicNavbarElements = globalState.loggedIn
 		? [
 				<NavbarItem key={1} router={router} route={"/member/status"}>
 					Status
 				</NavbarItem>,
 				<Link key={2} href={"/logout"}>
-					<a className="under-hover mx-[15px] text-[14px] font-sans">Logout</a>
+					<a className="under-hover mx-4">Logout</a>
 				</Link>,
 		  ]
 		: [
 				<NavbarItem key={1} router={router} route={"/login"}>
-					<span className="mx-[15px] text-[14px] font-sans">Login</span>
+					<span className="mx-4">Login</span>
 				</NavbarItem>,
 				<NavbarItem key={2} router={router} route={"/register"}>
-					<span className="mx-[15px] text-[14px] font-sans">Register</span>
+					<span className="mx-4">Register</span>
 				</NavbarItem>,
 		  ];
 
 	return (
-		<div className="h-[72px] p-[5px] w-full bg-primary-darker font-inter drop-shadow-lg text-white text-xl">
-			<div className="grid grid-cols-2 max-w-[1140px] h-full w-full mx-auto">
+		<div className="h-[4.5rem] p-1 w-full bg-primary-darker font-inter drop-shadow-lg text-white text-xl">
+			<div className="grid grid-cols-2 h-full w-full max-w-[1140px] mx-auto">
 				<div className="flex items-center">
 					<Link href="/">
 						<div className="h-full flex items-center cursor-pointer">
 							<Image src="/img/logo.png" width={40} height={40} />
-							<h1 className="ml-[5px] font-bold">Portal</h1>
+							<h1 className="ml-1 font-bold">Portal</h1>
 						</div>
 					</Link>
 				</div>
-				<div className="flex items-center justify-end">
+				<div className="flex items-center justify-end font-sans text-[14px]">
 					<NavbarItem router={router} route={"/"}>
 						Home
 					</NavbarItem>
 					<NavbarItem router={router} route={"/events"}>
 						Events
 					</NavbarItem>
-					{right_navbar_elements}
+					{dynamicNavbarElements}
 				</div>
 			</div>
 		</div>
