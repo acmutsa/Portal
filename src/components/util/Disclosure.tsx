04a0634f68c925fbceeb30851a88fa358d525815
Xevion <ryanchwalters@gmail.com>
2022-11-07 14:38:24 -0600

    Use legible font, lower opacity & better spacing for Disclosure

--- src/components/util/Disclosure.tsx
+++ src/components/util/Disclosure.tsx
@@ -2,7 +2,7 @@ import type { FunctionComponent } from "react";
 
 const Disclosure: FunctionComponent = () => {
 	return (
-		<p className="footer w-full text-center text-[10px] mx-auto text-white absolute bottom-0">
+		<p className="footer w-full font-inter opacity-[60%] text-center text-[12px] mx-auto text-white absolute bottom-0 py-1">
 			Made with &lt;/&gt; @ ACM UTSA
 			<br />© Association of Computing Machinery at UTSA {new Date().getFullYear()}. All Rights
 			Reserved.
