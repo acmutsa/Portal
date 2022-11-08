a071dbca31f5d6f26a23003b28d4434ec6092206
Xevion <ryanchwalters@gmail.com>
2022-11-07 23:01:19 -0600

    Inline EventDescription, server-side w/ abs. URL for QRCode
    I'll probably refactor out everything I just inlined, but I know
    I don't want two separate components for the event description,
    and I know I'm doing a major enough refactor for this.
    
    The QRCode data needed to be made server-side as hydration was faulty.
    Seems like the client side doesn't have access to process environment
    variables, so it of course generated something different for the
    QR Code data. Likewise, I just made it server side.
    
    I also adjusted a bunch of the tailwind classes to use the REM-based
    values instead of pixels. 4px = 0.25rem or thing-1 for Tailwind.
    
    This commit is NOT done, I just need to commit everything now.
    
    Also, serverProps is now "props"; felt there was no need to qualify
    that, it just makes everything complicated.

--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import EventHeader from "@/components/events/EventHeader";
-import EventDescription from "@/components/events/EventDescription";
 import Disclosure from "@/components/util/Disclosure";
 import { prisma } from "@/server/db/client";
 import useOpenGraph from "@/components/common/useOpenGraph";
 import OpenGraph from "@/components/common/OpenGraph";
-import { generateGoogleCalendarLink } from "@/utils/helpers";
+import { absUrl, generateGoogleCalendarLink } from "@/utils/helpers";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import Link from "next/link";
+import { SiGooglecalendar } from "react-icons/si";
+import QRCode from "react-qr-code";
 
 interface eventPageParams {
 	params: { id: string };
@@ -25,34 +28,35 @@ interface eventPageServerProps {
 	headerImage: string | null;
 	startDate: string | null;
 	endDate: string | null;
+	qrcodeData: string;
 }
 
-const EventView: NextPage<eventPageServerProps> = (serverProps) => {
+const EventView: NextPage<eventPageServerProps> = (props) => {
 	const router = useRouter();
 	const { id } = router.query;
 
 	const ogp = useOpenGraph({
-		title: serverProps.name ?? "Something",
-		description: `Come and join ${serverProps.organization} for ${serverProps.name}!`,
-		image: serverProps.headerImage ? {
-			url: serverProps.headerImage,
+		title: props.name ?? "Something",
+		description: `Come and join ${props.organization} for ${props.name}!`,
+		image: props.headerImage ? {
+			url: props.headerImage,
 			alt: "",
 			type: "image/png"
 		} : null,
 		url: `/events/${id}`,
 	});
 
-	if (serverProps.found) {
+	if (props.found) {
 		/*
 		* TODO: Location has to be a concrete location on Google Maps
 		*   The description should integrate the characteristics of the event (where/when).
 		*  */
 		const calendarLink = generateGoogleCalendarLink(
-			serverProps.name!,
-			serverProps.description ?? `Come join us for ${serverProps.name}`,
-			serverProps.location!,
-			new Date(serverProps.startDate!),
-			new Date(serverProps.endDate!)
+			props.name!,
+			props.description ?? `Come join us for ${props.name}`,
+			props.location!,
+			new Date(props.startDate!),
+			new Date(props.endDate!)
 		);
 
 		return (
@@ -61,21 +65,51 @@ const EventView: NextPage<eventPageServerProps> = (serverProps) => {
 					<title>{ogp.title}</title>
 					<OpenGraph properties={ogp} />
 				</Head>
-				<div className="page-view bg-darken pt-[20px]">
-					<EventHeader
-						title={serverProps.name || ""}
-						imageURL={serverProps.headerImage || ""}
-						eventHost={serverProps.organization || ""}
-						startDate={new Date(serverProps.startDate || "")}
-						endDate={new Date(serverProps.endDate || "")}
-						location={serverProps.location || ""}
-					/>
-					<br />
-					<EventDescription
-						description={serverProps.description || `Come and join us for ${serverProps.name}!`}
-						calanderLink={calendarLink}
-						eventID={typeof id === "string" ? id : "error"}
-					/>
+				<div className="page-view bg-darken pt-5">
+					<div
+						className="bg-white mx-auto min-h-[400px] rounded-xl max-w-[1200px]"
+					>
+						<div
+							className="flex items-center justify-center overflow-hidden rounded-l-xl bg-cover bg-no-repeat"
+							style={{ backgroundImage: `url(${props.headerImage})` }}
+						/>
+					</div>
+					<div className="mt-5 bg-white mx-auto max-w-[1200px] min-h-[25rem] rounded-xl p-3">
+						<div className="grid grid-cols-4 w-full min-h-[25rem]">
+							<div className="col-span-3 pr-4 py-5">
+								<div className="prose prose-md max-w-none font-raleway font-semibold">
+									<h2 className="border-b-2 mb-1">Description</h2>
+									<ReactMarkdown remarkPlugins={[remarkGfm]}>{props.description ?? ""}</ReactMarkdown>
+									<h2 className="border-b-2 mb-1">About ACM</h2>
+									<p>
+										ACM is the premier organization on campus for students interested in technology. ACM
+										is dedicated to providing members with opportunities for professional, academic, and
+										social growth outside the classroom in order to prepare students for their career in
+										tech or fuel their interest in the tech field. Anyone who has an interest in
+										technology can join ACM.
+									</p>
+								</div>
+							</div>
+							<div className="border-l-2">
+								<h2 className="text-center font-bold">Actions</h2>
+								<Link href={`/check-in/${id}`}>
+									<button className="h-12 w-full bg-primary-darker text-white rounded-lg font-semibold m-2">
+										Check-in
+									</button>
+								</Link>
+								<a href={calendarLink} target="_blank">
+									<button className="h-12 w-full bg-primary-lighter text-white rounded-lg font-semibold m-2 flex items-center justify-center">
+										<SiGooglecalendar className="mr-2 w-5 h-5" />
+										Add To Google Calendar
+									</button>
+								</a>
+								<QRCode
+									className="mx-auto scale-75"
+									value={props.qrcodeData}
+								/>
+							</div>
+						</div>
+					</div>
 					<Disclosure />
 				</div>
 			</>
@@ -85,9 +119,8 @@ const EventView: NextPage<eventPageServerProps> = (serverProps) => {
 	}
 };
 
-export async function getStaticProps(urlParams: eventPageParams) {
-	const params = urlParams.params;
-	const revalidationTime = 2;
+const revalidationTime = 2;
+export async function getStaticProps({ params }: eventPageParams) {
 
 	const event = await prisma.event.findUnique({
 		where: {
@@ -114,6 +147,7 @@ export async function getStaticProps(urlParams: eventPageParams) {
 			location: event.location,
 			startDate: event.eventStart.toString(),
 			endDate: event.eventEnd.toString(),
+			qrcodeData: absUrl(`/check-in/${params.id}`)
 		},
 		// Next.js will attempt to re-generate the page:
 		// - When a request comes in
