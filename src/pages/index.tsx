fcaad6498ba39dd88179d70ee261a18a8b12b533
Liam Murray <liam@murray.to>
2022-11-08 13:21:38 -0600

    Add Text To Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,10 +22,20 @@ const Home: NextPage = () => {
 			</Head>
 			<div className="page-view bg-darken pt-[25vh]">
 				<div className="w-full max-w-[1024px] h-full flex flex-col items-center mx-auto">
-					<div className="flex flex-col items-center justify-start w-[25rem] bg-white rounded">
-						<div className="shrink relative bottom-[5rem] h-[4.5rem]">
-							<Image className="drop-shadow-sm" width={150} height={150} src={"/img/logo.png"} />
+					<div className="flex flex-col items-center justify-start w-[25rem] bg-white rounded border-2 border-primary-lighter">
+						<div className="shrink relative bottom-[5rem] h-[140px] w-[140px] rounded-full overflow-hidden border-2 border-primary-lighter bg-white">
+							<Image
+								className="drop-shadow-sm"
+								width={150}
+								height={150}
+								src={"/img/logo.png"}
+								layout="fill"
+								objectFit="cover"
+							/>
 						</div>
+						<h1 className="text-5xl font-extrabold font-inter my-[10px] text-center text-primary-lighter relative bottom-[5rem]">
+							Membership Portal
+						</h1>
 						<div className="grid grid-rows-3 grid-cols-1 gap-2 px-3 pb-4 w-full text-white font-semibold">
 							<Link href="/events/">
 								<button className="h-[3.125rem] w-full px-2 justify-self-center bg-primary-darker rounded">
