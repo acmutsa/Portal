308ec70d2bb479bc511e2557b0bc56012d4f5c23
Xevion <ryanchwalters@gmail.com>
2022-11-09 17:16:44 -0600

    Show event count, improve time format, time cell dynamic format distinct
    The time cells now have a distinct formatting from other cells,
    showing that their display is "dynamically generated" and their value
    is "special" in a way that users can understand implicitly.
    
    Also, a helper function was added for pluralizing logic. It's just
    something I've come to find helpful when dealing with counting when
    rendering values.

--- src/components/admin/EventView.tsx
+++ src/components/admin/EventView.tsx
@@ -1,134 +1,139 @@
 import React, { FunctionComponent } from "react";
 import {
-	createColumnHelper,
-	flexRender,
-	getCoreRowModel,
-	useReactTable,
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable
 } from "@tanstack/react-table";
 import { format } from "date-fns";
 import { trpc } from "@/utils/trpc";
 import type { Event } from "@prisma/client";
+import { pluralize } from "@/utils/helpers";
 
 const columnHelper = createColumnHelper<Event>();
 
 const formatDateCell = (value: Date) => {
-	const hoverText = format(value, "EEEE, LLL do, yyyy");
-	const shortText = format(value, "y/MM/dd h:mm:dda");
-	return (
-		<span className="whitespace-nowrap" title={hoverText}>
+  const hoverText = format(value, "EEEE, LLL do, yyyy 'at' h:mm:ss aaaa");
+  const shortText = format(value, "y/MM/dd h:mma z");
+  return (
+    <span className="whitespace-nowrap bg-slate-300 p-1 rounded-lg font-medium" title={hoverText}>
 			{shortText}
 		</span>
-	);
+  );
 };
 
 const columns = [
-	columnHelper.accessor("name", {
-		header: "Name",
-		size: 200,
-	}),
-	columnHelper.accessor("description", {
-		header: "Description",
-		minSize: 200,
-		maxSize: 500,
-		cell: (info) => (
-			<div className="text-ellipsis overflow-hidden max-h-[5rem]">{info.getValue()}</div>
-		),
-	}),
-	columnHelper.accessor("organization", {
-		header: "Org.",
-		minSize: 30,
-		size: 30,
-	}),
-	columnHelper.accessor("eventStart", {
-		header: "Start",
-		cell: (info) => formatDateCell(info.getValue()),
-	}),
-	columnHelper.accessor("eventEnd", {
-		header: "End",
-		cell: (info) => formatDateCell(info.getValue()),
-	}),
-	columnHelper.accessor("formOpen", {
-		header: "Form Open",
-		cell: (info) => formatDateCell(info.getValue()),
-	}),
-	columnHelper.accessor("formClose", {
-		header: "Form Close",
-		cell: (info) => formatDateCell(info.getValue()),
-	}),
+  columnHelper.accessor("name", {
+    header: "Name",
+    size: 200
+  }),
+  columnHelper.accessor("description", {
+    header: "Description",
+    minSize: 200,
+    maxSize: 500,
+    cell: (info) => (
+      <div className="text-ellipsis overflow-hidden max-h-[5rem]">{info.getValue()}</div>
+    )
+  }),
+  columnHelper.accessor("organization", {
+    header: "Org.",
+    minSize: 30,
+    size: 30
+  }),
+  columnHelper.accessor("eventStart", {
+    header: "Start",
+    cell: (info) => formatDateCell(info.getValue())
+  }),
+  columnHelper.accessor("eventEnd", {
+    header: "End",
+    cell: (info) => formatDateCell(info.getValue())
+  }),
+  columnHelper.accessor("formOpen", {
+    header: "Form Open",
+    cell: (info) => formatDateCell(info.getValue())
+  }),
+  columnHelper.accessor("formClose", {
+    header: "Form Close",
+    cell: (info) => formatDateCell(info.getValue())
+  })
 ];
 
 const EventView: FunctionComponent = () => {
-	const events = trpc.useQuery(["events.getAll"]);
+  const events = trpc.useQuery(["events.getAll"]);
 
-	const table = useReactTable({
-		data: events.isSuccess ? events.data : [],
-		columns,
-		getCoreRowModel: getCoreRowModel(),
-		columnResizeMode: "onChange",
-		enableColumnResizing: true,
-	});
+  const table = useReactTable({
+    data: events.isSuccess ? events.data : [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    columnResizeMode: "onChange",
+    enableColumnResizing: true
+  });
 
-	return (
-		<div className="w-full h-full">
-			<div className="p-4 bg-white rounded">
-				<div className="pb-2">
-					{events.isSuccess ? (
-						<span className="pl-1 tracking-wide text-zinc-800 font-bold font-inter">Events</span>
-					) : (
-						<div className="animate-pulse p-2 h-5 bg-gray-300 rounded-full dark:bg-gray-700 w-48" />
-					)}
-				</div>
-				<table className="text-sm">
-					<thead>
-						{table.getHeaderGroups().map((headerGroup) => (
-							<tr key={headerGroup.id}>
-								{headerGroup.headers.map((header) => (
-									<th
-										{...{
-											key: header.id,
-											colSpan: header.colSpan,
-											style: {
-												width: header.getSize(),
-											},
-										}}
-									>
-										{header.isPlaceholder
-											? null
-											: flexRender(header.column.columnDef.header, header.getContext())}
-										<div
-											{...{
-												onMouseDown: header.getResizeHandler(),
-												onTouchStart: header.getResizeHandler(),
-												className: `resizer ${header.column.getIsResizing() ? "isResizing" : ""}`,
-											}}
-										/>
-									</th>
-								))}
-							</tr>
-						))}
-					</thead>
-					<tbody>
-						{table.getRowModel().rows.map((row) => (
-							<tr key={row.id}>
-								{row.getVisibleCells().map((cell) => (
-									<td
-										{...{
-											key: cell.id,
-											style: {
-												width: cell.column.getSize(),
-											},
-										}}
-									>
-										{flexRender(cell.column.columnDef.cell, cell.getContext())}
-									</td>
-								))}
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full h-full">
+      <div className="p-4 bg-white rounded">
+        <div className="pb-2">
+          {events.isSuccess ? (
+            <div className="flex justify-start font-inter">
+              <span className="pl-1 text-lg tracking-wide text-zinc-800 font-bold">Events</span>
+              <span
+                className="ml-6 px-2 text-zinc-600 text-xs my-auto">viewing {events.data.length} event{pluralize(events.data.length)}</span>
+            </div>
+          ) : (
+            <div className="animate-pulse p-2 h-5 bg-gray-300 rounded-full dark:bg-gray-700 w-48" />
+          )}
+        </div>
+        <table className="text-sm">
+          <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th
+                  {...{
+                    key: header.id,
+                    colSpan: header.colSpan,
+                    style: {
+                      width: header.getSize()
+                    }
+                  }}
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(header.column.columnDef.header, header.getContext())}
+                  <div
+                    {...{
+                      onMouseDown: header.getResizeHandler(),
+                      onTouchStart: header.getResizeHandler(),
+                      className: `resizer ${header.column.getIsResizing() ? "isResizing" : ""}`
+                    }}
+                  />
+                </th>
+              ))}
+            </tr>
+          ))}
+          </thead>
+          <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  {...{
+                    key: cell.id,
+                    style: {
+                      width: cell.column.getSize()
+                    }
+                  }}
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default EventView;
