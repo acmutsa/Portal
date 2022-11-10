db787683e0ec6f9c786b8d6847180c120ef02662
Liam Murray <liam@murray.to>
2022-11-10 09:14:51 -0600

    Use Env Variables For Config Options
    ADMIN_UNAME,
    ADMIN_PASS,
    EVENT_PAGE_REVALIDATION_TIME

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import settings from "@/config/settings.json";
+import { env } from "@/env/server.mjs";
 
 export function middleware(req: NextRequest) {
 	if (req.nextUrl.pathname.startsWith("/admin")) {
 		let username = req.cookies.get("admin_uname");
 		let password = req.cookies.get("admin_pass");
-		if (password != settings.admin.password || username != settings.admin.username) {
+		if (password != env.ADMIN_UNAME || username != env.ADMIN_PASS) {
 			if (password == undefined && username == undefined) {
 				return NextResponse.rewrite(new URL("/admin/login", req.url));
 			} else {
