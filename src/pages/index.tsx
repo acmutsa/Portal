36d6d2553dc1ef49292ddcc9977823779a815b27
Liam Murray <liam@murray.to>
2022-11-10 08:59:06 -0600

    Update Home Page Styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
 				<OpenGraph properties={ogp} />
 			</Head>
 			<div className="page-view bg-darken pt-[25vh]">
-				<div className="w-full max-w-[1024px] h-full flex flex-col items-center mx-auto">
+				<div className="w-full max-w-[1024px] h-full flex flex-col items-center mx-auto m-[5px]">
 					<div className="flex flex-col items-center justify-start w-[25rem] bg-white rounded border-2 border-primary-lighter">
 						<div className="shrink relative bottom-[5rem] h-[140px] w-[140px] rounded-full overflow-hidden border-2 border-primary-lighter bg-white">
 							<Image
@@ -33,8 +33,10 @@ const Home: NextPage = () => {
 								objectFit="cover"
 							/>
 						</div>
-						<h1 className="text-xl sm:text-3xl md:text-4xl font-extrabold font-inter my-[10px] text-center text-primary-lighter relative bottom-[5rem]">
-							Membership Portal
+						<h1 className="text-5xl font-extrabold font-inter my-[10px] text-center text-primary-lighter relative bottom-[5rem]">
+							Membership
+							<br />
+							Portal
 						</h1>
 						<div className="grid grid-rows-3 grid-cols-1 gap-2 px-3 pb-4 w-full text-white font-semibold">
 							<Link href="/events/">
