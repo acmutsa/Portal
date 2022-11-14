cc15e1a22214a2c1602486d89efd302ccc980e92
Xevion <ryanchwalters@gmail.com>
2022-11-13 18:55:56 -0600

    Restyle and cleanup check-in card interaction

--- src/pages/check-in/[id].tsx
+++ src/pages/check-in/[id].tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
 import { prisma } from "@/server/db/client";
-import Link from "next/link";
 
 interface eventPageParams {
 	params: { id: string };
@@ -10,8 +8,7 @@ interface eventPageParams {
 	defaultLocale: string;
 }
 
-interface eventPageServerProps {
-	found: boolean;
+interface Event {
 	name: string | null;
 	description: string | null;
 	organization: string | null;
@@ -21,25 +18,25 @@ interface eventPageServerProps {
 	endDate: string | null;
 }
 
-const EventView: NextPage<eventPageServerProps> = (serverProps) => {
-	const router = useRouter();
-	const { id } = router.query;
-	if (serverProps.found) {
+const EventView: NextPage<{ event: Event | null }> = ({ event }) => {
+	if (event) {
 		return (
-			<div className="page-view bg-darken pt-[20px]">
-				<div className="bg-white max-w-[800px] mx-auto min-h-[400px] rounded-xl text-center flex flex-col items-center justify-center p-[10px]">
-					<h1 className="text-5xl font-extrabold font-inter">Thanks For Attending</h1>
-					<h1 className="text-5xl font-extrabold font-inter">
-						<span className="text-primary-lighter">{serverProps.name}</span>!
+			<div className="page-view bg-darken">
+				<div className="mt-10 mx-auto max-w-[30rem] mx-auto bg-white rounded-lg text-center flex flex-col items-center justify-center">
+					<h1 className="text-2xl p-1.5 font-inter font-bold text-primary-darker">
+						Check in to <span className="text-primary-lighter">{event.name}</span>?
 					</h1>
-					<button className="h-[50px] w-full bg-primary-darker text-white rounded-lg font-semibold mx-[5px] mb-[5px] max-w-[300px] mt-[40px]">
-						Check-in
+					<div className="px-4 mb-4 w-full max-w-[30rem] m-1">
+						<textarea
+							className="w-full font-roboto h-full min-h-[5rem] rounded ring-1 ring-zinc-300 resize-y placeholder:mx-2"
+							placeholder="  Leave your feedback here..."
+						/>
+					</div>
+
+					<h1 className="text-5xl font-extrabold font-inter"></h1>
+					<button className="h-[40px] mb-3 w-full bg-primary-darker font-inter text-white rounded font-semibold max-w-[15rem]">
+						Submit
 					</button>
-					<Link href={`/events/${id}`}>
-						<button className="h-[50px] w-full bg-primary-lighter text-white rounded-lg font-semibold mx-[5px] mb-[5px] max-w-[300px] mt-[5px]">
-							View Event Details
-						</button>
-					</Link>
 				</div>
 			</div>
 		);
@@ -50,53 +47,40 @@ const EventView: NextPage<eventPageServerProps> = (serverProps) => {
 
 export async function getStaticProps(urlParams: eventPageParams) {
 	const params = urlParams.params;
-	const revalTime = 2;
-
 	const event = await prisma.event.findUnique({
 		where: {
 			pageID: params.id.toLowerCase(),
 		},
 	});
 
-	if (event == null) {
+	if (event == null)
 		return {
 			props: {
-				found: false,
+				event: null,
 			},
-			revalidate: revalTime,
+			revalidate: 5,
 		};
-	}
 
 	return {
 		props: {
-			found: true,
-			name: event.name,
-			description: event.description,
-			organization: event.organization,
-			headerImage: event.headerImage,
-			location: event.location,
-			startDate: event.eventStart.toString(),
-			endDate: event.eventEnd.toString(),
+			event: {
+				name: event.name,
+				description: event.description,
+				organization: event.organization,
+				headerImage: event.headerImage,
+				location: event.location,
+				startDate: event.eventStart.toString(),
+				endDate: event.eventEnd.toString(),
+			},
 		},
 		// Next.js will attempt to re-generate the page:
 		// - When a request comes in
 		// - At most once every 10 seconds
-		revalidate: revalTime, // In seconds
+		revalidate: 2, // In seconds
 	};
 }
 
 export async function getStaticPaths() {
-	// const res = await fetch('https://.../posts')
-	// const posts = await res.json()
-
-	// Get the paths we want to pre-render based on posts
-	// const paths = posts.map((post) => ({
-	//   params: { id: post.id },
-	// }))
-
-	// We'll pre-render only these paths at build time.
-	// { fallback: blocking } will server-render pages
-	// on-demand if the path doesn't exist.
 	return { paths: [], fallback: "blocking" };
 }
 
