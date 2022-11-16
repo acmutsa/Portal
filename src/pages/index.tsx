387dd346bb6e66a79b88ecc37370ef6e751f3030
Xevion <ryanchwalters@gmail.com>
2022-11-16 14:35:00 -0600

    Improve index card logo placement and viewport responsive handling
    - Footer is now unselectable. No point. Feels nicer in the background.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,18 +20,19 @@ const Home: NextPage = () => {
 				<title>{ogp.title}</title>
 				<OpenGraph properties={ogp} />
 			</Head>
-			<div className="page-view bg-darken !pt-40 md:!pt-44 lg:!pt-48">
-				<div className="w-full max-w-[1024px] h-full flex flex-col items-center mx-auto px-4">
-					<div className="flex flex-col items-center justify-start max-w-[25rem] w-full sm:w-[25rem] bg-white rounded border-2 border-primary-light">
-						<div className="shrink relative text-sm md:text-base bottom-[0em] h-[9em] w-[9em] rounded-full overflow-hidden border-2 border-primary-light bg-white">
-							<Image
-								className="drop-shadow-sm"
-								src={"/img/logo.png"}
-								layout="fill"
-								objectFit="cover"
-							/>
-						</div>
-						<h1 className="text-4xl md:text-[2.75rem] md:leading-12  lg:text-5xl font-extrabold font-inter my-[10px] text-center text-primary-light relative bottom-[0rem]">
+			<div className="page-view bg-darken">
+				{/* TODO: Fix the logo's weird overlap/clipping issues at short viewports */}
+				<div className="w-full !mt-4 md:!mt-6 lg:!mt-10 max-w-[1024px] h-full flex flex-col items-center mx-auto px-4">
+					<div className="relative z-20 md:text-base -bottom-16 h-[144px] w-[144px] rounded-full overflow-visible border-2 border-primary-light bg-white">
+						<Image
+							className="absolute overflow-visible drop-shadow-sm"
+							src={"/img/logo.png"}
+							layout="fill"
+							objectFit="cover"
+						/>
+					</div>
+					<div className="flex flex-col z-10 relative overflow-hidden items-center justify-start max-w-[25rem] w-full sm:w-[25rem] bg-white rounded border-2 border-primary-light">
+						<h1 className="text-4xl md:text-[2.75rem] md:leading-12 pt-14 lg:text-5xl font-extrabold font-inter my-[10px] text-center text-primary-light relative bottom-[0rem]">
 							Membership
 							<br />
 							Portal
