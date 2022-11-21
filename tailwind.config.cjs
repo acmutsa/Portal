e80243e2f328f199e78921a81ca9e6e101c70c6b
Xevion <ryanchwalters@gmail.com>
2022-11-20 19:27:50 -0600

    Add more shadow-inner versions in Tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,15 @@ module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			boxShadow: {
+				'inner-md': 'inset 1px 4px 6px 0 rgb(0 0 0 / 0.1)',
+				'inner-md-2': 'inset 2px 2px 6px 0 rgb(0 0 0 / 0.15)',
+				'inner-md-3': 'inset 2px 4px 6px 0 rgb(0 0 0 / 0.21)',
+				'inner-md-4': 'inset 2px 4px 10px 0 rgb(0 0 0 / 0.28)',
+				'inner-lg': 'inset 4px 5px 7px 0 rgb(0 0 0 / 0.2)',
+				'inner-xl': 'inset 4px 9px 9px 0 rgb(0 0 0 / 0.3)',
+				'inner-2xl': 'inset 4px 11px 12px 0 rgb(0 0 0 / 0.3)'
+			},
 			screens: {
 				"xs": "450px",
 			},
