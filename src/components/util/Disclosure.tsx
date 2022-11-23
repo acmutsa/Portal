3b4b5e6897531215eb253bb1976cbf599661b88e
Liam Murray <liam@murray.to>
2022-11-23 13:23:06 -0600

    Add Github Link To Disclosure

--- src/components/util/Disclosure.tsx
+++ src/components/util/Disclosure.tsx
@@ -3,7 +3,15 @@ import type { FunctionComponent } from "react";
 const Disclosure: FunctionComponent = () => {
 	return (
 		<p className="footer w-full font-inter opacity-[60%] text-center select-none text-[12px] mx-auto text-white absolute bottom-0 py-1">
-			Made with <span className="disclosure-symbol">&lt;/&gt;</span> & ♥ @ ACM UTSA |{" "}
+			Made with{" "}
+			<a
+				href="https://github.com/UTSA-ACM/Portal"
+				target={"_blank"}
+				className="disclosure-symbol underline"
+			>
+				&lt;/&gt;
+			</a>{" "}
+			& ♥ @ ACM UTSA |{" "}
 			<a href="https://forms.gle/eqj1gMRRhZKitAn47" target={"_blank"} className="underline">
 				Report a Bug
 			</a>
