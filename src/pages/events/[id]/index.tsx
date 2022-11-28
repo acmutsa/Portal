c9ede6739a36b4c303028051310b0be91547450d
Xevion <ryanchwalters@gmail.com>
2022-11-28 14:36:00 -0600

    Merge Watch on Twitch button into revamped Event page

--- src/pages/events/[id]/index.tsx
+++ src/pages/events/[id]/index.tsx
@@ -15,6 +15,7 @@ import BigEventHeader from "@/components/events/BigEventHeader";
 import QRCode from "react-qr-code";
 import NoSSR from "@/components/common/NoSSR";
 import { env } from "@/env/server.mjs";
+import { Event } from "@prisma/client";
 
 interface eventPageParams {
 	params: { id: string };
@@ -23,130 +24,108 @@ interface eventPageParams {
 	defaultLocale: string;
 }
 
-interface eventPageServerProps {
-	found: boolean;
-	name: string | null;
-	description: string | null;
-	organization: string | null;
-	location: string | null;
-	headerImage: string | null;
-	startDate: string | null;
-	endDate: string | null;
-	qrcodeData: string;
-}
-
-const EventView: NextPage<eventPageServerProps> = (props) => {
+const EventView: NextPage<{ event: Event; qrcodeData: string }> = ({ event, qrcodeData }) => {
 	const router = useRouter();
 	const { id } = router.query;
 
-	const startDate = props.startDate ? new Date(props.startDate) : null;
-	const endDate = props.endDate ? new Date(props.endDate) : null;
-
 	const ogp = useOpenGraph({
-		title: (props.name ?? "Something") + " | ACM",
-		description: `Come and join ${props.organization} for ${props.name}!`,
-		image: props.headerImage
+		title: (event.name ?? "Something") + " | ACM",
+		description: `Come and join ${event.organization} for ${event.name}!`,
+		image: event.headerImage
 			? {
-					url: props.headerImage,
+					url: event.headerImage,
 					alt: "",
 					type: "image/png",
 			  }
 			: null,
 		url: `/events/${id}`,
-		labels: props.found
-			? [
-					["When", format(startDate!, "E, MM/dd/yyyy h:mma")],
-					["Where", props.location!],
-			  ]
-			: undefined,
+		labels: [
+			["When", format(event.eventStart, "E, MM/dd/yyyy h:mma")],
+			["Where", event.location!],
+		],
 	});
 
-	if (props.found) {
-		const formatString = "h:mmaaaaaa";
-		const startString = lightFormat(startDate!, formatString);
-		const endString = lightFormat(endDate!, formatString);
-		const calendarLink = generateGoogleCalendarLink(
-			new Date(props.startDate!),
-			new Date(props.endDate!),
-			props.name,
-			`Location: ${props.location}\nWhen: ${startString} to ${endString}\n\n${
-				props.description ?? `Come join us for ${props.name}`
-			}`,
-			props.location
-		);
+	const formatString = "h:mmaaaaaa";
+	const startString = lightFormat(event.eventStart, formatString);
+	const endString = lightFormat(event.eventEnd, formatString);
+	const calendarLink = generateGoogleCalendarLink(
+		event.eventStart,
+		event.eventEnd,
+		event.name,
+		`Location: ${event.location}\nWhen: ${startString} to ${endString}\n\n${
+			event.description ?? `Come join us for ${event.name}`
+		}`,
+		event.location
+	);
 
-		return (
-			<>
-				<Head>
-					<title>{ogp.title}</title>
-					<OpenGraph properties={ogp} />
-				</Head>
-				<div className="page-view bg-darken !pt-[100px]">
-					<BigEventHeader
-						title={props.name || "Error"}
-						imageURL={props.headerImage || "Error"}
-						hostOrg={props.organization || "ACM"}
-						startDate={new Date(props.startDate!) || new Date()}
-						endDate={new Date(props.endDate!) || new Date()}
-						location={props.location || "Error"}
-					/>
-					<div className="mt-5 bg-white mx-auto max-w-[1200px] min-h-[25rem] rounded-xl p-3">
-						<div className="grid grid-cols-4 w-full min-h-[25rem]">
-							<div className="col-span-3 pr-4 py-2">
-								<div className="mx-3 prose prose-md max-w-none font-raleway font-semibold">
-									<h2 className="border-b-2 mb-1">Description</h2>
-									{props.description !== null && props.description.length > 0 ? (
-										<ReactMarkdown className="ml-3 [&>*]:my-0" remarkPlugins={[remarkGfm]}>
-											{props.description!}
-										</ReactMarkdown>
-									) : (
-										<p className="ml-3 text-zinc-500 font-medium">
-											No description was provided for this event.
-										</p>
-									)}
-									<h2 className="border-b-2 mb-1 mt-4">About ACM</h2>
-									<p className="ml-3">
-										ACM is the premier organization on campus for students interested in technology.
-										ACM is dedicated to providing members with opportunities for professional,
-										academic, and social growth outside the classroom in order to prepare students
-										for their career in tech or fuel their interest in the tech field. Anyone who
-										has an interest in technology can join ACM.
+	return (
+		<>
+			<Head>
+				<title>{ogp.title}</title>
+				<OpenGraph properties={ogp} />
+			</Head>
+			<div className="page-view bg-darken !pt-[100px]">
+				<BigEventHeader
+					title={event.name || "Error"}
+					imageURL={event.headerImage || "Error"}
+					hostOrg={event.organization || "ACM"}
+					startDate={event.eventEnd}
+					endDate={event.eventStart}
+					location={event.location || "Error"}
+				/>
+				<div className="mt-5 bg-white mx-auto max-w-[1200px] min-h-[25rem] rounded-xl p-3">
+					<div className="grid grid-cols-4 w-full min-h-[25rem]">
+						<div className="col-span-3 pr-4 py-2">
+							<div className="mx-3 prose prose-md max-w-none font-raleway font-semibold">
+								<h2 className="border-b-2 mb-1">Description</h2>
+								{event.description !== null && event.description.length > 0 ? (
+									<ReactMarkdown className="ml-3 [&>*]:my-0" remarkPlugins={[remarkGfm]}>
+										{event.description!}
+									</ReactMarkdown>
+								) : (
+									<p className="ml-3 text-zinc-500 font-medium">
+										No description was provided for this event.
 									</p>
-								</div>
-							</div>
-							<div className="border-l-2">
-								<h2 className="text-center font-bold">Actions</h2>
-								<Link href={`/events/${id}/check-in`}>
-									<button className="h-12 w-full bg-primary text-white rounded-lg font-semibold mx-2 flex items-center justify-center mb-2">
-										Check-in
-									</button>
-								</Link>
-								<Link href={calendarLink} target="_blank">
-									<button className="h-12 w-full bg-secondary text-white rounded-lg font-semibold mx-2 flex items-center justify-center mb-2">
-										<SiGooglecalendar className="mr-2 w-5 h-5" />
-										Add To Google Calendar
-									</button>
-								</Link>
-								<Link href={"https://twitch.tv/acmutsa"} target="_blank">
-									<button className="h-12 w-full bg-[#9146FF] text-white rounded-lg font-semibold mx-2 flex items-center justify-center">
-										<SiTwitch className="mr-2 w-5 h-5" />
-										Watch on Twitch
-									</button>
-								</Link>
-								<br />
-								<NoSSR>
-									<QRCode className="mx-auto scale-75" value={props.qrcodeData} />
-								</NoSSR>
+								)}
+								<h2 className="border-b-2 mb-1 mt-4">About ACM</h2>
+								<p className="ml-3">
+									ACM is the premier organization on campus for students interested in technology.
+									ACM is dedicated to providing members with opportunities for professional,
+									academic, and social growth outside the classroom in order to prepare students for
+									their career in tech or fuel their interest in the tech field. Anyone who has an
+									interest in technology can join ACM.
+								</p>
 							</div>
 						</div>
+						<div className="border-l-2">
+							<h2 className="text-center font-bold">Actions</h2>
+							<Link href={`/events/${id}/check-in`}>
+								<button className="h-12 w-full bg-primary text-white rounded-lg font-semibold m-2">
+									Check-in
+								</button>
+							</Link>
+							<a href={calendarLink} target="_blank">
+								<button className="h-12 w-full bg-secondary text-white rounded-lg font-semibold m-2 flex items-center justify-center">
+									<SiGooglecalendar className="mr-2 w-5 h-5" />
+									Add To Google Calendar
+								</button>
+							</a>
+							<a href={"https://twitch.tv/acmutsa"} target="_blank">
+								<button className="h-12 w-full bg-[#9146FF] text-white rounded-lg font-semibold mx-2 flex items-center justify-center">
+									<SiTwitch className="mr-2 w-5 h-5" />
+									Watch on Twitch
+								</button>
+							</a>
+							<NoSSR>
+								<QRCode className="mx-auto scale-75" value={qrcodeData} />
+							</NoSSR>
+						</div>
 					</div>
-					<Disclosure />
 				</div>
-			</>
-		);
-	} else {
-		return <p>Not found</p>;
-	}
+				<Disclosure />
+			</div>
+		</>
+	);
 };
 
 const revalidationTime = Math.max(env.EVENT_PAGE_REVALIDATION_TIME, 20);
@@ -160,44 +139,21 @@ export async function getStaticProps({ params }: eventPageParams) {
 
 	if (event == null) {
 		return {
-			props: {
-				found: false,
-			},
+			notFound: true,
 			revalidate: revalidationTime,
 		};
 	}
 
 	return {
 		props: {
-			found: true,
-			name: event.name,
-			description: event.description,
-			organization: event.organization,
-			headerImage: event.headerImage,
-			location: event.location,
-			startDate: event.eventStart.toString(),
-			endDate: event.eventEnd.toString(),
+			event: event,
 			qrcodeData: absUrl(`/events/${params.id}/check-in`),
 		},
-		// Next.js will attempt to re-generate the page:
-		// - When a request comes in
-		// - At most once every 10 seconds
-		revalidate: revalidationTime, // In seconds
+		revalidate: revalidationTime,
 	};
 }
 
 export async function getStaticPaths() {
-	// const res = await fetch('https://.../posts')
-	// const posts = await res.json()
-
-	// Get the paths we want to pre-render based on posts
-	// const paths = posts.map((post) => ({
-	//   params: { id: post.id },
-	// }))
-
-	// We'll pre-render only these paths at build time.
-	// { fallback: blocking } will server-render pages
-	// on-demand if the path doesn't exist.
 	return { paths: [], fallback: "blocking" };
 }
 
