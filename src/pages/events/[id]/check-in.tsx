8d44b4210eb0f0b181c5024fcca607813a4722ba
Xevion <ryanchwalters@gmail.com>
2022-11-28 14:21:54 -0600

    Add authentication navigation guard to check-in page

--- src/pages/events/[id]/check-in.tsx
+++ src/pages/events/[id]/check-in.tsx
@@ -1,8 +1,9 @@
-import type { NextPage } from "next";
 import { Event, prisma } from "@/server/db/client";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import { trpc } from "@/utils/trpc";
+import { useGlobalContext } from "@/components/common/GlobalContext";
+import { NextPage } from "next";
 
 interface eventPageParams {
 	params: { id: string };
@@ -18,6 +19,8 @@ interface FormValues {
 const CheckinView: NextPage<{ event: Event | null }> = ({ event }) => {
 	const checkin = trpc.useMutation(["member.checkin"]);
 	const router = useRouter();
+	const [globalState] = useGlobalContext();
+
 	const { register, handleSubmit } = useForm({
 		defaultValues: { feedback: "" },
 	});
@@ -37,33 +40,34 @@ const CheckinView: NextPage<{ event: Event | null }> = ({ event }) => {
 		);
 	};
 
-	if (event) {
-		return (
-			<div className="page-view bg-darken">
-				<div className="mt-10 max-w-[30rem] mx-auto bg-white rounded-lg text-center flex flex-col items-center justify-center">
-					<h1 className="text-2xl p-1.5 font-inter font-bold text-primary">
-						Thanks for attending <span className="text-secondary">{event.name}</span>?
-					</h1>
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<div className="px-4 mb-4 w-full max-w-[30rem] m-1">
-							<textarea
-								className="w-full font-roboto h-full min-h-[5rem] rounded ring-1 ring-zinc-300 resize-y placeholder:mx-2"
-								placeholder="  Leave your feedback here..."
-								{...register("feedback", { pattern: /^[\w.+\-!@#$%^&*(),;':\[\]~]*$/ })}
-							/>
-						</div>
-
-						<h1 className="text-5xl font-extrabold font-inter"></h1>
-						<button className="h-[40px] mb-3 w-full bg-primary font-inter text-white rounded font-semibold max-w-[15rem]">
-							Submit
-						</button>
-					</form>
-				</div>
-			</div>
-		);
+	if (globalState.ready && !globalState.member) {
+		router.push("/login");
+		return <div className="page-view bg-darken" />;
 	}
 
-	return <p>Not found</p>;
+	return (
+		<div className="page-view bg-darken">
+			<div className="mt-10 max-w-[30rem] mx-auto bg-white rounded-lg text-center flex flex-col items-center justify-center">
+				<h1 className="text-2xl p-1.5 font-inter font-bold text-primary">
+					Thanks for attending <span className="text-secondary">{event.name}</span>?
+				</h1>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className="px-4 mb-4 w-full max-w-[30rem] m-1">
+						<textarea
+							className="w-full font-roboto h-full min-h-[5rem] rounded ring-1 ring-zinc-300 resize-y placeholder:mx-2"
+							placeholder="  Leave your feedback here..."
+							{...register("feedback", { pattern: /^[\w.+\-!@#$%^&*(),;':\[\]~]*$/ })}
+						/>
+					</div>
+
+					<h1 className="text-5xl font-extrabold font-inter"></h1>
+					<button className="h-[40px] mb-3 w-full bg-primary font-inter text-white rounded font-semibold max-w-[15rem]">
+						Submit
+					</button>
+				</form>{" "}
+			</div>
+		</div>
+	);
 };
 
 export async function getStaticProps(urlParams: eventPageParams) {
