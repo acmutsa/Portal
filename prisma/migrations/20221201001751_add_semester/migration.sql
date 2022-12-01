c6d4513e0d50bbbd4b81abe0f62c7b244d844873
Liam Murray <liam@murray.to>
2022-11-30 18:18:07 -0600

    Add Semester Column

---
+++ prisma/migrations/20221201001751_add_semester/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Checkin" ADD COLUMN     "semester" TEXT NOT NULL DEFAULT 'Fall 2022';
+
+-- AlterTable
+ALTER TABLE "Event" ADD COLUMN     "semester" TEXT NOT NULL DEFAULT 'Fall 2022';
