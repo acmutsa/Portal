0d68c98907c2ca490b60d3e3e7c8742db13b6bfe
Xevion <ryanchwalters@gmail.com>
2022-11-30 21:35:02 -0600

    Add state, handling & events to Sort component

--- src/components/events/Sort.tsx
+++ src/components/events/Sort.tsx
@@ -1,22 +1,27 @@
 import { Menu, Transition } from "@headlessui/react";
 import { BiChevronDown } from "react-icons/bi";
-import { Fragment } from "react";
+import { Dispatch, Fragment, useMemo } from "react";
 import { classNames } from "@/utils/helpers";
 
-interface SortOption {
-	id: string;
-	label: string;
-}
-
 interface SortProps {
-	options: SortOption[];
+	label?: string;
+	options: Record<string, string>;
+	onChange?: Dispatch<string>;
 }
 
-const Sort = ({ options }: SortProps) => {
+const Sort = ({ label, options, onChange }: SortProps) => {
+	const onChangeHandler = useMemo(() => {
+		return (value: string) => {
+			return () => {
+				if (onChange != null) onChange(value);
+			};
+		};
+	}, [onChange]);
+
 	return (
 		<>
 			<Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-				Sort
+				{label ?? "Sort"}
 				<BiChevronDown
 					className="flex-shrink-0 -mr-1 ml-1 h-5 w-5 text-gray-400 group-hover:text-gray-500"
 					aria-hidden="true"
@@ -33,17 +38,18 @@ const Sort = ({ options }: SortProps) => {
 			>
 				<Menu.Items className="origin-top-left absolute left-0 z-10 mt-2 w-40 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
 					<div className="py-1">
-						{options.map((option) => (
-							<Menu.Item key={option.id}>
+						{Object.entries(options).map(([id, label]) => (
+							<Menu.Item key={id}>
 								{({ active }) => (
-									<a
+									<button
+										onClick={onChangeHandler(id)}
 										className={classNames(
 											active ? "bg-gray-100" : "",
 											"block px-4 py-2 text-sm font-medium text-gray-900"
 										)}
 									>
-										{option.label}
-									</a>
+										{label}
+									</button>
 								)}
 							</Menu.Item>
 						))}
