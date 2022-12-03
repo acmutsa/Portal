8a8ed14544070c21100f6327bf621cecc04a48dc
Xevion <ryanchwalters@gmail.com>
2022-12-02 18:13:19 -0600

    Re-center WarningDialog on mobile viewports
    Not trying to spam commits, I just pushed before noticing the issue
    on mobile viewports.

--- src/components/member/WarningDialog.tsx
+++ src/components/member/WarningDialog.tsx
@@ -24,7 +24,7 @@ const WarningDialog: FunctionComponent<DialogProps> = (props: DialogProps) => {
 				initialFocus={cancelButtonRef}
 				onClose={onClose}
 			>
-				<div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+				<div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 					<Transition.Child
 						as={Fragment}
 						enter="ease-out duration-300"
