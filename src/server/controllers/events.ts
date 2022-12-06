b48afb23ec054b31fed78eb0714a0e0c58b3f22d
Xevion <ryanchwalters@gmail.com>
2022-12-06 09:52:48 -0600

    Add organization filtering, greatly improve composition of Prisma filters
    Much happier with this syntax now that I know that it's valid.

--- src/server/controllers/events.ts
+++ src/server/controllers/events.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
 import { prisma } from "@/server/db/client";
-import { setProperty } from "dot-prop";
 import { OrganizationEnum } from "@/utils/transform";
 
 export const SortOptionEnum = z.enum(["recent", "attendance"]);
@@ -25,13 +24,30 @@ export async function getEvents(filters?: EventFilter) {
 		if (sort == SortOptionEnum.enum.recent) orderObject = { eventEnd: "asc" };
 		if (sort == SortOptionEnum.enum.attendance) orderObject = { checkins: { _count: "desc" } };
 
-		// TODO: Organization filtering logic
 		// TODO: Semester filtering logic
-		let whereObject = {};
-		if (filters.past !== true)
-			if (filters.past === false)
-				whereObject = setProperty(whereObject, "eventEnd.gte", new Date());
-			else whereObject = setProperty(whereObject, "eventEnd.gte", filters.past);
+		filters.past = filters.past ?? false;
+		filters.organizations = filters.organizations ?? [];
+
+		const whereObject = {
+			AND: [
+				filters.past !== true
+					? { eventEnd: { gte: filters.past === false ? new Date() : filters.past } }
+					: null,
+				filters.organizations.length > 0
+					? {
+							OR: filters.organizations?.map((organizationName) => ({
+								organization: { equals: organizationName },
+							})),
+					  }
+					: null,
+			],
+		};
+
+		// Remove any null filters from composition
+		whereObject.AND = whereObject.AND.filter((v) => v != null);
+
+		// Development only:
+		// console.log(inspect(whereObject, { showHidden: false, depth: null, colors: true }));
 
 		return await prisma.event.findMany({
 			orderBy: orderObject,
