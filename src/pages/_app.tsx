550c778ab4b829456c6d0c721ed37476eaea6e33
Xevion <ryanchwalters@gmail.com>
2022-12-07 21:44:35 -0600

    Refactor globals.css with Sass & separate animations file
    - New package: sass

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
 import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
 import { loggerLink } from "@trpc/client/links/loggerLink";
 import { withTRPC } from "@trpc/next";
