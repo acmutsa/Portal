47a492e7613eb42c45b17af85b659c2a80f42ca9
Xevion <ryanchwalters@gmail.com>
2023-01-24 20:54:08 -0600

    Remove unused field 'semester' on Checkin

---
+++ prisma/migrations/20230125025340_remove_unused_field_checkin_semester/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `semester` on the `Checkin` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Checkin" DROP COLUMN "semester";
