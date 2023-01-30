dd1fb13971b24f92bdc34d0fa3b242ac43cfa209
Xevion <xevioni@yandex.com>
2023-01-30 03:59:39 -0600

    Update admin middleware to use new cookies object `value` property

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,8 @@ import { cookies } from "@/utils/constants";
 
 export function middleware(req: NextRequest) {
 	if (req.nextUrl.pathname.startsWith("/admin")) {
-		let username = req.cookies.get(cookies.admin_username);
-		let password = req.cookies.get(cookies.admin_password);
+		let username = req.cookies.get(cookies.admin_username)?.value;
+		let password = req.cookies.get(cookies.admin_password)?.value;
 		if (password != env.ADMIN_PASS || username != env.ADMIN_UNAME) {
 			if (password == undefined && username == undefined) {
 				return NextResponse.rewrite(new URL("/admin/login", req.url));
