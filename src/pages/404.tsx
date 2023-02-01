0813182cee4accc8323900bbe243f94fcd9f8c15
Xevion <xevioni@yandex.com>
2023-02-01 04:52:46 -0600

    Fix 404 with RootLayout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,12 @@
+import RootLayout from "@/components/layout/RootLayout";
+
 export default function Custom404() {
 	return (
-		<div className="page-view bg-darken text-white flex items-center justify-center">
+		<RootLayout innerClassName="text-white flex items-center justify-center">
 			<div className="flex-inline justify-center items-center mb-20 font-inter p-3 bg-gray-900/0 [&>*]:drop-shadow-xl">
 				<span className="text-3xl font-semibold mx-3">404</span>
 				<span className="text-xl">This page could not be found.</span>
 			</div>
-		</div>
+		</RootLayout>
 	);
 }
