1118e72968fadcca6e29a2688a033568cb2f2f23
Xevion <xevioni@yandex.com>
2023-02-01 18:11:18 -0600

    Add SSR validation helper: validateAdmin()

--- src/utils/server_helpers.ts
+++ src/utils/server_helpers.ts
@@ -5,6 +5,7 @@ import { NextApiRequestCookies } from "next/dist/server/api-utils";
 import { getMember } from "@/server/controllers/member";
 import { MemberData } from "@prisma/client";
 import { cookies } from "@/utils/constants";
+import { env } from "@/env/server.mjs";
 
 export type NextServerRequest = IncomingMessage & { cookies: NextApiRequestCookies };
 
@@ -37,4 +38,28 @@ export async function validateMember(
 
 	// Otherwise, it's valid, return the member.
 	return [true, member];
+}
+
+export async function validateAdmin(
+	request: NextServerRequest,
+	response: ServerResponse
+): Promise<boolean> {
+	// Acquire admin credentials
+	const username = request.cookies[cookies.admin_username]?.toLowerCase();
+	const password = request.cookies[cookies.admin_password]?.toLowerCase();
+
+	if (username == null || password == null) {
+		if (username != null) deleteCookie(cookies.admin_username, { req: request, res: response });
+		else if (password != null)
+			deleteCookie(cookies.admin_password, { req: request, res: response });
+	}
+
+	// If the provided credentials do not match, delete them.
+	if (username != env.ADMIN_UNAME || password != env.ADMIN_PASS) {
+		deleteCookie(cookies.admin_username, { req: request, res: response });
+		deleteCookie(cookies.admin_password, { req: request, res: response });
+		return false;
+	}
+
+	return true;
 }
