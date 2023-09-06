b6a494692adbe913f39573734af74b7990d3e196
Seligmann <zachary.a.seligman@gmail.com>
2023-09-06 06:21:10 -0500

    Added multi-select, cleaner HTML for displaying sets of items, added back OpenGraph

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,23 +1,54 @@
 import type { NextPage } from "next";
-import { Widget } from "@typeform/embed-react";
-import useOpenGraph from "@/components/common/useOpenGraph";
-import OpenGraph from "@/components/common/OpenGraph";
 import Head from "next/head";
-import RootLayout from "@/components/layout/RootLayout";
+import OpenGraph from "@/components/common/OpenGraph";
+import useOpenGraph from "@/components/common/useOpenGraph";
 import { useForm } from "react-hook-form";
 import { useGlobalContext } from "@/components/common/GlobalContext";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import RootLayout from "@/components/layout/RootLayout";
+import { trpc } from "@/utils/trpc";
+import { EthnicityType, IdentityType, OrganizationType } from "@/utils/transform";
+import { z } from "zod";
 
 const Join: NextPage = () => {
-	const { register, handleSubmit } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			organizations: z.set(OrganizationType).optional(),
+			ethnicities: z.set(EthnicityType),
+			identity: z.set(IdentityType.or(z.string())).optional(),
+			name: "",
+			major: "",
+			email: "",
+			abc123: "",
+			classification: OrganizationType,
+			graduationDate: "",
+			birthday: "",
+			shirtType: "",
+			addressLineTwo: "",
+			addressLineOne: "",
+			shirtSize: "",
+			zipcode: "",
+			city: "",
+			state: "",
+			country: "",
+			otherGender: "",
+		},
+	});
 	const [selectedOtherGender, setSelectedOtherGender] = useState(false);
 	const [file, setFile] = useState(null);
 	const [globalState, setGlobalState] = useGlobalContext();
 	const router = useRouter();
-	const onSubmit = (data: any) => {
-		console.log(data);
+	const createProfile = trpc.member.createProfile.useMutation();
+
+	const onSubmit = async (data: any) => {
+		createProfile.mutate({ ...data });
 	};
+	const onInvalid = (errors: any) => console.error(errors);
 
 	const onFileChange = (data: any) => {
 		console.log(`file changed. data: ${data}`);
@@ -25,8 +56,8 @@ const Join: NextPage = () => {
 	};
 
 	const handleGenderInput = (data: any) => {
-		if (data.target.value === "other" && selectedOtherGender) return setSelectedOtherGender(false);
-		if (data.target.value === "other" && !selectedOtherGender) return setSelectedOtherGender(true);
+		if (data.target.value === "other" && selectedOtherGender) setSelectedOtherGender(false);
+		if (data.target.value === "other" && !selectedOtherGender) setSelectedOtherGender(true);
 	};
 
 	useEffect(() => {
@@ -39,14 +70,42 @@ const Join: NextPage = () => {
 		url: "/register",
 	});
 
-	const shirtSizes: Map<string, string> = new Map();
-	shirtSizes.set("extraSmall", "XS");
-	shirtSizes.set("small", "S");
-	shirtSizes.set("medium", "M");
-	shirtSizes.set("large", "L");
-	shirtSizes.set("extraLarge", "XL");
-	shirtSizes.set("doubleExtraLarge", "2XL");
-	shirtSizes.set("tripleExtraLarge", "3XL");
+	const shirtSizes: Map<string, string> = new Map([
+		["extraSmall", "XS"],
+		["small", "S"],
+		["medium", "M"],
+		["large", "L"],
+		["extraLarge", "XL"],
+		["doubleExtraLarge", "2XL"],
+		["tripleExtraLarge", "3XL"],
+	]);
+
+	const clubs: Map<string, string> = new Map([
+		["ACM", "ACM"],
+		["ACM_W", "ACM-W"],
+		["ROWDY_CREATORS", "Rowdy Creators"],
+		["CODING_IN_COLOR", "Coding in Color"],
+		["ICPC", "ICPC"],
+	]);
+
+	const identity: Map<string, string> = new Map([
+		["MALE", "Male"],
+		["FEMALE", "Female"],
+		["NON_BINARY", "Non-binary"],
+		["TRANSGENDER", "Transgender"],
+		["INTERSEX", "Intersex"],
+		["DOES_NOT_IDENTIFY", "Prefer not to say"],
+		["other", "Let me type..."],
+	]);
+
+	const ethnicities: Map<string, string> = new Map([
+		["africanAmericanOrBlack", "African American or Black"],
+		["asian", "Asian"],
+		["nativeAmericanOrAlaskanNative", "Native American or Alaskan Native"],
+		["nativeHawaiianOrPacificIslander", "Native Hawaiian or Pacific Islander"],
+		["hispanicOrLatinx", "Hispanic or Latinx"],
+		["white", "White"],
+	]);
 
 	return (
 		<>
@@ -54,15 +113,15 @@ const Join: NextPage = () => {
 				<title>{ogp.title}</title>
 				<OpenGraph properties={ogp} />
 			</Head>
-			<RootLayout>
+			<RootLayout className={"flex justify-center items-center"}>
 				<div className="p-3 pt-8">
-					<div className="bg-white max-w-[25rem] self-center p-3 rounded-xl text-center flex flex-col items-center justify-center">
+					<div className="bg-white max-w-[25rem] p-3 rounded-xl ">
 						<p className="text-lg md:text-[22px] tracking-wide md:tracking-normal font-semibold text-slate-700 font-raleway mb-2">
 							Register
 						</p>
 						<form
-							onSubmit={handleSubmit(onSubmit)}
-							className="min-w-[20rem] sm:w-full flex flex-col font-inter justify-start p-1 pb-2 text-left"
+							onSubmit={handleSubmit(onSubmit, onInvalid)}
+							className="min-w-[20rem] sm:w-full flex flex-col font-inter justify-start p-1 pb-2 text-left "
 						>
 							<label className="">
 								<p className="block tracking-wide text-sm md:text-base font-medium text-slate-700">
@@ -70,8 +129,8 @@ const Join: NextPage = () => {
 								</p>
 								<input
 									type="text"
-									id="fullName"
-									{...register("fullName", { required: true, maxLength: 180 })}
+									id="name"
+									{...register("name", { required: true, maxLength: 180 })}
 									className="login peer px-3 py-1.5 md:py-2 lg:py-2.5 bg-white border shadow-sm border-slate-300
 									 block w-full rounded-md md:text-base placeholder-slate-400"
 								/>
@@ -127,11 +186,11 @@ const Join: NextPage = () => {
 								Classification
 							</span>
 							<select {...register("classification", { required: true })}>
-								<option value="freshman">Freshman</option>
-								<option value="sophomore">Sophomore</option>
-								<option value="junior">Junior</option>
-								<option value="senior">Senior</option>
-								<option value="graduate">Graduate</option>
+								<option value="FRESHMAN">Freshman</option>
+								<option value="SOPHOMORE">Sophomore</option>
+								<option value="JUNIOR">Junior</option>
+								<option value="SENIOR">Senior</option>
+								<option value="Unknown">Graduate</option>
 							</select>
 							<p className="mb-1 text-[13px] invisible peer-invalid:visible text-pink-600 text-xs sm:text-sm">
 								classification
@@ -176,35 +235,25 @@ const Join: NextPage = () => {
 							</p>
 
 							<span className="block text-sm md:text-base font-medium text-slate-700">Gender</span>
-							<fieldset {...register("gender", { required: true })} onInput={handleGenderInput}>
-								<input type="checkbox" id="male" name="male" value="male" />
-								<label htmlFor="male">Male</label>
-								<br />
-								<input type="checkbox" id="female" name="female" value="female" />
-								<label htmlFor="female">Female</label>
-								<br />
-								<input type="checkbox" id="nonBinary" name="nonBinary" value="nonBinary" />
-								<label htmlFor="nonBinary">Non-binary</label>
-								<br />
-								<input type="checkbox" id="transgender" name="transgender" value="transgender" />
-								<label htmlFor="transgender">Transgender</label>
-								<br />
-								<input type="checkbox" id="intersex" name="intersex" value="intersex" />
-								<label htmlFor="intersex">Intersex</label>
-								<br />
-								<input
-									type="checkbox"
-									id="preferNotToSay"
-									name="preferNotToSay"
-									value="preferNotToSay"
-								/>
-								<label htmlFor="preferNotToSay">Prefer not to say</label>
-								<br />
-								<input type="checkbox" id="other" name="other" value="other" />
-								<label htmlFor="other">Let me type...</label>
-							</fieldset>
+							{Array.from(identity.entries()).map((entry) => {
+								const [genderAbbrev, gender] = entry;
+								return (
+									<>
+										<div>
+											<input
+												{...register("identity")}
+												type="checkbox"
+												value={genderAbbrev}
+												onChange={handleGenderInput}
+											/>
+											{gender}
+										</div>
+									</>
+								);
+							})}
+
 							<p className="mb-1 text-[13px] invisible peer-invalid:visible text-pink-600 text-xs sm:text-sm">
-								gender
+								Gender
 							</p>
 							{selectedOtherGender && (
 								<label>
@@ -212,11 +261,7 @@ const Join: NextPage = () => {
 										Gender
 									</span>
 									<input
-										type="gender"
-										id="gender"
-										{...register("gender", {
-											required: true,
-										})}
+										{...register("otherGender", { required: true })}
 										className="login peer px-3 py-1.5 md:py-2 lg:py-2.5 bg-white border shadow-sm border-slate-300
 									 block w-full rounded-md md:text-base placeholder-slate-400"
 									/>
@@ -226,128 +271,55 @@ const Join: NextPage = () => {
 								</label>
 							)}
 
+							<span className="block text-sm md:text-base font-medium text-slate-700">Clubs</span>
+							{Array.from(clubs.entries()).map((entry) => {
+								const [club, clubName] = entry;
+								return (
+									<>
+										<div>
+											<input {...register("organizations")} type="checkbox" value={club} />
+											{clubName}
+										</div>
+									</>
+								);
+							})}
+
 							<span className="block text-sm md:text-base font-medium text-slate-700">
 								Ethnicity
 							</span>
-							<fieldset {...register("ethnicity", { required: true })}>
-								<input
-									type="checkbox"
-									id="africanAmericanOrBlack"
-									name="africanAmericanOrBlack"
-									value="africanAmericanOrBlack"
-								/>
-								<label htmlFor="africanAmericanOrBlack">African American or Black</label>
-								<br />
-								<input type="checkbox" id="asian" name="asian" value="asian" />
-								<label htmlFor="asian">Asian</label>
-								<br />
-								<input
-									type="checkbox"
-									id="nativeAmericanOrAlaskanNative"
-									name="nativeAmericanOrAlaskanNative"
-									value="nativeAmericanOrAlaskanNative"
-								/>
-								<label htmlFor="nativeAmericanOrAlaskanNative">
-									Native American / Alaskan Native
-								</label>
-								<br />
-								<input
-									type="checkbox"
-									id="nativeHawaiianOrPacificIslander"
-									name="nativeHawaiianOrPacificIslander"
-									value="nativeHawaiianOrPacificIslander"
-								/>
-								<label htmlFor="nativeHawaiianOrPacificIslander">
-									Native Hawaiian or Pacific Islander
-								</label>
-								<br />
-								<input
-									type="checkbox"
-									id="hispanicOrLatinx"
-									name="hispanicOrLatinx"
-									value="hispanicOrLatinx"
-								/>
-								<label htmlFor="hispanicOrLatinx">Hispanic / Latinx</label>
-								<br />
-								<input type="checkbox" id="white" name="white" value="white" />
-								<label htmlFor="white">White</label>
-							</fieldset>
-
-							<span className="block text-sm md:text-base font-medium text-slate-700">
-								What organizations are you interested in?
-							</span>
-							<fieldset {...register("orgs", { required: true })}>
-								<input type="checkbox" id="acm" name="acm" value="acm" />
-								<label htmlFor="acm">ACM</label>
-								<br />
-								<input type="checkbox" id="acmW" name="acmW" value="acmW" />
-								<label htmlFor="acmW">ACM-W</label>
-								<br />
-								<input
-									type="checkbox"
-									id="rowdyCreators"
-									name="rowdyCreators"
-									value="rowdyCreators"
-								/>
-								<label htmlFor="rowdyCreators">Rowdy Creators</label>
-								<br />
-								<input type="checkbox" id="icpc" name="icpc" value="icpc" />
-								<label htmlFor="icpc">ICPC</label>
-								<br />
-								<input
-									type="checkbox"
-									id="codingInColor"
-									name="codingInColor"
-									value="codingInColor"
-								/>
-								<label htmlFor="codingInColor">Coding in Color</label>
-								<br />
-								<input
-									type="checkbox"
-									id="acmHackathons"
-									name="acmHackathons"
-									value="acmHackathons"
-								/>
-								<label htmlFor="acmHackathons">ACM Hackathons</label>
-							</fieldset>
-							<span className="block text-sm md:text-base font-medium text-slate-700">
-								Drop your resume here!
-							</span>
-							<div>
-								<input type={"file"} onChange={onFileChange} />
-							</div>
+							{Array.from(ethnicities.entries()).map((entry) => {
+								const [ethnicityType, ethnicity] = entry;
+								return (
+									<>
+										<div>
+											<input {...register("ethnicities")} type="checkbox" value={ethnicity} />
+											{ethnicity}
+										</div>
+									</>
+								);
+							})}
 
 							<span className="block text-sm md:text-base font-medium text-slate-700">
 								Shirt type
 							</span>
-							<fieldset {...register("shirtType", { required: true })}>
-								<input type="checkbox" id="unisex" name="unisex" value="unisex" />
-								<label htmlFor="unisex">Unisex</label>
-								<br />
-								<input type="checkbox" id="womens" name="womens" value="womens" />
-								<label htmlFor="womens">Womens</label>
-							</fieldset>
+							<select {...register("shirtType", { required: true })}>
+								<option value="unisex">Unisex</option>
+								<option value="womens">Womens</option>
+							</select>
 
 							<span className="block text-sm md:text-base font-medium text-slate-700">
 								Shirt size
 							</span>
-							<fieldset {...register("shirtSize", { required: true })}>
+							<select {...register("shirtSize", { required: true })}>
 								{Array.from(shirtSizes.entries()).map((entry) => {
 									const [shirtSizeType, shirtSize] = entry;
 									return (
 										<>
-											<input
-												type="checkbox"
-												id={shirtSizeType}
-												name={shirtSizeType}
-												value={shirtSizeType}
-											/>
-											<label htmlFor={shirtSizeType}>{shirtSize}</label>
-											<br />
+											<option value={shirtSizeType}>{shirtSize}</option>
 										</>
 									);
 								})}
-							</fieldset>
+							</select>
 
 							<label>
 								<span className="block text-sm md:text-base font-medium text-slate-700">
@@ -357,7 +329,6 @@ const Join: NextPage = () => {
 									id="addressLineOne"
 									{...register("addressLineOne", {
 										required: true,
-										pattern: /\b\d+\s+[a-zA-Z0-9\s.-]+\b/i,
 									})}
 									className="login peer px-3 py-1.5 md:py-2 lg:py-2.5 bg-white border shadow-sm border-slate-300
 									 block w-full rounded-md md:text-base placeholder-slate-400"
@@ -376,24 +347,31 @@ const Join: NextPage = () => {
 								/>
 							</label>
 							<label>
-								<span className="block text-sm md:text-base font-medium text-slate-700">
-									City, Region
-								</span>
+								<span className="block text-sm md:text-base font-medium text-slate-700">City</span>
 								<input
-									id="cityAndRegion"
-									{...(register("cityAndRegion"), { required: true })}
+									id="city"
+									{...register("city", { required: true })}
 									className="login peer px-3 py-1.5 md:py-2 lg:py-2.5 bg-white border shadow-sm border-slate-300
 									 block w-full rounded-md md:text-base placeholder-slate-400"
 								/>
 							</label>
 
 							<label>
+								<span className="block text-sm md:text-base font-medium text-slate-700">State</span>
+								<input
+									id="state"
+									{...register("state", { required: true })}
+									className="login peer px-3 py-1.5 md:py-2 lg:py-2.5 bg-white border shadow-sm border-slate-300
+									 block w-full rounded-md md:text-base placeholder-slate-400"
+								/>
+							</label>
+							<label>
 								<span className="block text-sm md:text-base font-medium text-slate-700">
 									Postal code
 								</span>
 								<input
-									id="postalCode"
-									{...(register("postalCode"), { required: true })}
+									id="zipcode"
+									{...register("zipcode", { required: true })}
 									className="login peer px-3 py-1.5 md:py-2 lg:py-2.5 bg-white border shadow-sm border-slate-300
 									 block w-full rounded-md md:text-base placeholder-slate-400"
 								/>
@@ -405,12 +383,13 @@ const Join: NextPage = () => {
 								</span>
 								<input
 									id="country"
-									{...(register("country"), { required: true })}
+									{...register("country", { required: true })}
 									className="login peer px-3 py-1.5 md:py-2 lg:py-2.5 bg-white border shadow-sm border-slate-300
 									 block w-full rounded-md md:text-base placeholder-slate-400"
 								/>
 							</label>
 							<button
+								type={"submit"}
 								className="bg-sky-500 focus:bg-sky-600 hover:bg-sky-600 px-5 outline-none py-2 mt-1.5 md:mt-0 sm:py-2.5 text-sm
 							 leading-5 rounded-md font-semibold text-white"
 							>
